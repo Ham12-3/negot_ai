a6d63834bffd35d8f11f18fd93c1a01e84d69959
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-10-23 17:01:03 +0100

    update

--- client/src/app/(private)/dashboard/layout.tsx
+++ client/src/app/(private)/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Layout({children}: {children:React.ReactNode}){
     return (
         <ProtectedLayout>
-        <main>
+        <main className="flex-1 overflow-x-hidden bg-white overflow-y-auto p-6">
         {children}
         </main>
         
--- client/src/components/dashboard/protected-layout.tsx
+++ client/src/components/dashboard/protected-layout.tsx
@@ -1,29 +1,60 @@
-
-"use client"
+"use client";
 
 import { useCurrentUser } from "@/hooks/use-current-user";
-import { Loader2 } from "lucide-react";
+import { Loader2, LockIcon } from "lucide-react";
 import React from "react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { Button } from "../ui/button";
 
-export function ProtectedLayout({children}: {children: React.ReactNode}) {
-    const {user, isLoading} = useCurrentUser()
+export function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  const { user, isLoading } = useCurrentUser();
 
-    if(isLoading) {
-        return (
-            <div className="flex items-center justify-centerh-screen">
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-centerh-screen">
+        <div className="flex items-center justify-center">
+          <Loader2 className="size-4 mr-2 animate-spin" />
+        </div>
+      </div>
+    );
+  }
+  if (!user) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <AuthCard />
+      </div>
+    );
+  }
 
-                <div className="flex items-center justify-center">
-<Loader2 className="size-4 mr-2 animate-spin"/>
-                </div>
+  return <>{children}</>;
+}
 
+export default function AuthCard() {
+  return (
+    <Card className="w-full max-w-3xl mx-auto">
+      <div className="flex flex-col sm:flex-row">
+        <div className="sm:w-1/4 bg-primary/10 flex items-center justify-center p-4">
+          <LockIcon className="size-16 text-primary" />
+        </div>
+
+        <div className="sm:w-3/4 p-4">
+          <CardHeader className="space-y-1 px-0 pb-2">
+            <CardTitle className="text-2xl font-bold">Authentication required </CardTitle>
+
+            <CardDescription>
+                You need to be logged in to access this page.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="px-0 py-2">
+            <div className="flex flex-col sm:flex-row gap-2">
+<Button className="flex-1 " variant={"outline"}>
+    Continue with Google
+</Button>
             </div>
-        )
-    }
-    if(!user) {
-        return (
-            <div>
 
-            </div>
-        )
-    }
+          </CardContent>
+        </div>
+      </div>
+    </Card>
+  );
 }
